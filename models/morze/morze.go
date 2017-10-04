6232720ddfe2847e46a7fe175623706816b710b4
terrabayteee <blak__ang@mail.ru>
2017-10-05 00:47:27 +0300

    description, rus_dict, reading json

--- models/morze/morze.go
+++ models/morze/morze.go
@@ -1,15 +1,38 @@
 package morze
 
 import (
-		"fmt"
-    "net/http"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, decoder(r.URL.Path));
+type Symb_code struct {
+	Symbol_code string
+	Symbol      string
+}
+
+type Dictionary struct {
+	Symb_codes []Symb_code
+}
+
+const dictFilename = "rus.json"
+
+func json_reader() Dictionary {
+	rawDataIn, err := ioutil.ReadFile(dictFilename)
+	if err != nil {
+		log.Fatal("Cannot load settings:", err)
+	}
+
+	var dictionary Dictionary
+	print(rawDataIn)
+	err = json.Unmarshal(rawDataIn, &dictionary)
+	if err != nil {
+		log.Fatal("Invalid settings format:", err)
+	}
+	return dictionary
 }
 
 func decoder(buffer string) string {
-    fmt.Println(buffer);
-    return "morze";
+	dictionary := json_reader()
+	return "morze"
 }
